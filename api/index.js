553889ebb3d3605fc0be5dbd0f80ecec4b344394
yenilikci <m.melihcelik017@outlook.com>
2021-08-11 17:17:15 +0300

    Add necessary middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const app = express();
 const port = process.env.port || 5000;
+
+/* middlewares */
+//express apps by setting various HTTP headers
+app.use(helmet());
+//method,path,statusCode,ms,message
+app.use(morgan("dev"));
+//working with json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
   console.log(`api ${port} port numarası ile ayakta`);
 });
